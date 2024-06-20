ddcb4e8c13b2565b005f157c820aff55ab6153de
nmuggleton <naomi.muggleton@wbs.ac.uk>
2024-06-20 13:38:04 +0100

    clear output

--- calculate_price_per_unit/main.sh
+++ calculate_price_per_unit/main.sh
@@ -7,25 +7,28 @@ export PASS="/home/wbs/bsstng/.config/sftp/credentials.txt"
 export LOCALDIR="/storage/wbs/bsstng/inequality"
 export REMOTEDIR="/Shared533--BARLEVIN/consumer_panel/kilts_scanner"
 
-# Import array for sbatch
-## Remove the old host key from the known_hosts file
-ssh-keygen -R "$HOST"
+## Import array for sbatch
+### Remove the old host key from the known_hosts file
+#ssh-keygen -R "$HOST"
+#
+### Attempt to connect and fetch the new host key
+#ssh-keyscan -H "$HOST" >> ~/.ssh/known_hosts
+#
+### Run the expect script to perform the sftp transfer
+#echo "Starting sftp file transfer..."
+#expect sftp_array_transfer.expect "$PASS" "$USER" "$HOST" "$REMOTEDIR"
+#
+## Load the necessary modules and run the R script
+#module purge
+#module load GCC/13.2.0
+#module load OpenMPI/4.1.6
+#module load R/4.3.3
+#
+## Run the R script to create the array
+#Rscript make_array.R
 
-## Attempt to connect and fetch the new host key
-ssh-keyscan -H "$HOST" >> ~/.ssh/known_hosts
-
-## Run the expect script to perform the sftp transfer
-echo "Starting sftp file transfer..."
-expect sftp_array_transfer.expect "$PASS" "$USER" "$HOST" "$REMOTEDIR"
-
-# Load the necessary modules and run the R script
-module purge
-module load GCC/13.2.0
-module load OpenMPI/4.1.6
-module load R/4.3.3
-
-R make_array.R
-
+# Run the slurm script
 sbatch data_processing_pipeline.slurm
 
-rm params/*.txt
+## Clean up the params folder
+#rm params/*.txt
